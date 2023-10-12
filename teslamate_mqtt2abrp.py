6671bb2ad083f2f25edad28f91b6ef8be4810b96
Juan Carlos Heredia <jchm@infoinnova.net>
2023-10-12 03:15:39 +0200

    Print response.text

--- teslamate_mqtt2abrp.py
+++ teslamate_mqtt2abrp.py
@@ -284,18 +284,16 @@ def updateABRP():
     json_data = json.dumps(data)
 
     # Now, json_string contains the JSON representation of the 'data' dictionary
-    msgDetails = "Data object sent:"
+    msgDetails = "Data object to send:"
     print(msgDetails, json_data)
 
     try:
         headers = {"Authorization": "APIKEY "+APIKEY}
         body = {"tlm": json_data}
         response = requests.post("https://api.iternio.com/1/tlm/send?token="+USERTOKEN, headers=headers, json=body)
-        resp = response.json()
-        if resp["status"] != "ok":
-            print("Response from ABRP:", response.text)
-    except json.JSONDecodeError as e:
-        print("JSON decode error:", e)
+        #resp = response.json()
+        #if resp["status"] != "ok":
+        print("Response from ABRP:", response.text)
     except Exception as ex:
         print("Unexpected exception while calling ABRP API:", sys.exc_info()[0])
         print(ex)
