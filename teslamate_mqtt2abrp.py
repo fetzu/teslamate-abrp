f3aaf7c0b5d26a76d61466fa5450c57bfc44a046
Juan Carlos Heredia <jchm@infoinnova.net>
2023-10-15 16:48:27 +0200

    Calculate acurrate power on AC charging

--- teslamate_mqtt2abrp.py
+++ teslamate_mqtt2abrp.py
@@ -216,8 +216,8 @@ def on_message(client, userdata, message):
             #print("Unneeded topic:", message.topic, payload)
 
         # Calculate acurrate power on AC charging
-        if data["is_charging"]==True and data["is_dcfc"]==False:
-            data["power"] = float(data["current"] * data["voltage"] * charger_phases) / 1000.0
+        if data["is_charging"]==True and data["is_dcfc"]==False and "voltage" in data and "current" in data:
+            data["power"] = float(data["current"] * data["voltage"] * charger_phases) / 1000.0 * -1
 
         return
 
