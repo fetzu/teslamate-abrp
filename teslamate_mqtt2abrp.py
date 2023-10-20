f3288193f465a5cad5554f2ff0cf60d20d31f7eb
Julien <6372605+fetzu@users.noreply.github.com>
2023-10-20 20:14:42 +0200

    Revert "Update response message"
    This reverts commit 3296d4e5237bf27afab8cb374d3e477723eed3ad.

--- teslamate_mqtt2abrp.py
+++ teslamate_mqtt2abrp.py
@@ -290,8 +290,6 @@ def updateABRP():
         resp = response.json()
         if resp["status"] != "ok":
             print("Response from ABRP:", response.text)
-        else:
-            print("Response from ABRP:", resp["status"])
     except Exception as ex:
         print("Unexpected exception while calling ABRP API:", sys.exc_info()[0])
         print(ex)
